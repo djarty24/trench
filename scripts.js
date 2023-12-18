e43cd401fd6b7b47c6097cae3a841719111e81a7
Revati Tambe <24revati@gmail.com>
2023-12-17 18:18:53 -0800

    presets!

--- scripts.js
+++ scripts.js
@@ -1,4 +1,3 @@
-let background = new Audio('/assets/audio/calm-boulevard.mp3');
 let rain = new Audio('/assets/audio/heavy-rain.mp3');
 let marching = new Audio('/assets/audio/marching.mp3');
 let drums = new Audio('/assets/audio/drums.mp3');
@@ -16,31 +15,59 @@ let slider5 = document.getElementById('slider5');
 let slider6 = document.getElementById('slider6');
 let slider7 = document.getElementById('slider7');
 let slider8 = document.getElementById('slider8');
-let slider9 = document.getElementById('slider9');
 
-slider1.oninput = function(){ background.volume = this.value/100; }
-slider2.oninput = function(){ rain.volume = this.value/100; }
-slider3.oninput = function(){ marching.volume = this.value/100; }
-slider4.oninput = function(){ drums.volume = this.value/100; }
-slider5.oninput = function(){ whistle.volume = this.value/100; }
-slider6.oninput = function(){ gunshots.volume = this.value/100; }
-slider7.oninput = function(){ artillery.volume = this.value/100; }
-slider8.oninput = function(){ gas.volume = this.value/100; }
-slider9.oninput = function(){ explosions.volume = this.value/100; }
+rain.volume = 0.1;
+marching.volume = 0.75;
+drums.volume = 0.65;
+whistle.volume = 0.5;
+gunshots.volume = 0.75;
+artillery.volume = 0.5;
+gas.volume = 1;
+explosions.volume = 0.5;
+
+slider1.oninput = function(){ rain.volume = this.value/100; }
+slider2.oninput = function(){ marching.volume = this.value/100; }
+slider3.oninput = function(){ drums.volume = this.value/100; }
+slider4.oninput = function(){ whistle.volume = this.value/100; }
+slider5.oninput = function(){ gunshots.volume = this.value/100; }
+slider6.oninput = function(){ artillery.volume = this.value/100; }
+slider7.oninput = function(){ gas.volume = this.value/100; }
+slider8.oninput = function(){ explosions.volume = this.value/100; }
 
 function toggleSound(file){
-    if (file === 1){
+    if (file === 0){
         if (document.getElementById('play' + file).className === "on"){
             document.getElementById('play' + file).className = "off";
             document.getElementById('pause' + file).className = "on";
-            background.play();
-            background.loop = true;
+            rain.play();
+            marching.play();
+            drums.play();
+            whistle.play();
+            gunshots.play();
+            artillery.play();
+            gas.play();
+            explosions.play();
+            rain.loop = true;
+            marching.loop = true;
+            drums.loop = true;
+            whistle.loop = true;
+            gunshots.loop = true;
+            artillery.loop = true;
+            gas.loop = true;
+            explosions.loop = true;
         } else if (document.getElementById('pause' + file).className === "on") {
             document.getElementById('pause' + file).className = "off";
             document.getElementById('play' + file).className = "on";
-            background.pause();
+            rain.pause();
+            marching.pause();
+            drums.pause();
+            whistle.pause();
+            gunshots.pause();
+            artillery.pause();
+            gas.pause();
+            explosions.pause();
         }
-    } else if (file === 2){
+    } else if (file === 1){
         if (document.getElementById('play' + file).className === "on"){
             document.getElementById('play' + file).className = "off";
             document.getElementById('pause' + file).className = "on";
@@ -51,7 +78,7 @@ function toggleSound(file){
             document.getElementById('play' + file).className = "on";
             rain.pause();
         }
-    } else if (file === 3){
+    } else if (file === 2){
         if (document.getElementById('play' + file).className === "on"){
             document.getElementById('play' + file).className = "off";
             document.getElementById('pause' + file).className = "on";
@@ -62,7 +89,7 @@ function toggleSound(file){
             document.getElementById('play' + file).className = "on";
             marching.pause();
         }
-    } else if (file === 4){
+    } else if (file === 3){
         if (document.getElementById('play' + file).className === "on"){
             document.getElementById('play' + file).className = "off";
             document.getElementById('pause' + file).className = "on";
@@ -73,7 +100,7 @@ function toggleSound(file){
             document.getElementById('play' + file).className = "on";
             drums.pause();
         }
-    } else if (file === 5){
+    } else if (file === 4){
         if (document.getElementById('play' + file).className === "on"){
             document.getElementById('play' + file).className = "off";
             document.getElementById('pause' + file).className = "on";
@@ -84,7 +111,7 @@ function toggleSound(file){
             document.getElementById('play' + file).className = "on";
             whistle.pause();
         }
-    } else if (file === 6){
+    } else if (file === 5){
         if (document.getElementById('play' + file).className === "on"){
             document.getElementById('play' + file).className = "off";
             document.getElementById('pause' + file).className = "on";
@@ -95,7 +122,7 @@ function toggleSound(file){
             document.getElementById('play' + file).className = "on";
             gunshots.pause();
         }
-    } else if (file === 7){
+    } else if (file === 6){
         if (document.getElementById('play' + file).className === "on"){
             document.getElementById('play' + file).className = "off";
             document.getElementById('pause' + file).className = "on";
@@ -106,7 +133,7 @@ function toggleSound(file){
             document.getElementById('play' + file).className = "on";
             artillery.pause();
         }
-    } else if (file === 8){
+    } else if (file === 7){
         if (document.getElementById('play' + file).className === "on"){
             document.getElementById('play' + file).className = "off";
             document.getElementById('pause' + file).className = "on";
@@ -117,7 +144,7 @@ function toggleSound(file){
             document.getElementById('play' + file).className = "on";
             gas.pause();
         }
-    } else if (file === 9){
+    } else if (file === 8){
         if (document.getElementById('play' + file).className === "on"){
             document.getElementById('play' + file).className = "off";
             document.getElementById('pause' + file).className = "on";
